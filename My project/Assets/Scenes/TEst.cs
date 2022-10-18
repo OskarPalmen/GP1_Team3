a6442f0a4b98bc384bd6051b95533acc26d4940e
OskarPalmen <oskarpalmen00@gmail.com>
2022-10-18 14:51:42 +0200

    Merch test
    we are trying out the merch test

--- My project/Assets/Scenes/TEst.cs
+++ My project/Assets/Scenes/TEst.cs
@@ -7,7 +7,7 @@ public class TEst : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello Motherfuckers");
+        Debug.Log("Hello Team");
     }
 
     // Update is called once per frame
