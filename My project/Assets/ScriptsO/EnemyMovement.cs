7bcab904cddfee3ccc9bf438f70f8b855531d32b
OskarPalmen <oskarpalmen00@gmail.com>
2022-10-19 16:38:29 +0200

    started to make enemy scripts
    this is the begining of an ennemy script wich is targeting the player.

---
+++ My project/Assets/ScriptsO/EnemyMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = this.transform.position;
+        position.y = position.y - 0.002f;
+        this.transform.position = position;
+    }
+}
