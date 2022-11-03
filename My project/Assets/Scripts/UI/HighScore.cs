e815738f5bb486ca4b4e382e0a6c602345280353
Chizinha <axel.g.ahlund@gmail.com>
2022-11-03 11:44:14 +0100

    High Score Calculation done
    Need to add a "spawn point" object to Scene for it to know where the start position is

--- My project/Assets/Scripts/UI/HighScore.cs
+++ My project/Assets/Scripts/UI/HighScore.cs
@@ -7,28 +7,34 @@ public class HighScore : MonoBehaviour
 {
     [Header("Component")]
     public TextMeshProUGUI highScore;
+    public Transform playerPos;
+    public Transform startPos;
 
     [Header("Values")]
+    public float multiplier = 10f;
     public float currentHighScore;
-    public float enemyScore;
-    public float progressScore;
+    public float enemyScore = 0f;
+    public float progressScore = 0f;
+    //public float progressStart; //use if changeing scene
 
 
     void Start()
     {
-        
+        playerPos = FindObjectOfType<CharacterController>().transform;
+        startPos = FindObjectOfType<SpawnPoint>().transform;
     }
 
     void Update()
     {
-        float currentProgress = 1f; //add distance calculation here
+        float currentProgress = Vector3.Distance(startPos.position, playerPos.position); //distance calculation here
+        currentProgress *= multiplier;
         if (currentProgress > progressScore)
         {
             progressScore = currentProgress;
         }
         currentHighScore = progressScore + enemyScore;
         highScore.text = currentHighScore.ToString("0");
-        AddScore(1);
+        //AddScore(1);
     }
 
     public void AddScore(int score)
