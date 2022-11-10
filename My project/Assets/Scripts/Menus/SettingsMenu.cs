37c97fb68fd505622f92e1a67f97077f07f77e6b
Mattiashelllstrom <116079599+Mattiashelllstrom@users.noreply.github.com>
2022-11-10 13:19:19 +0100

    Revert "Merge branch 'main' into mattias-branch-2-test"
    This reverts commit 14ddabcaf78e7afbdaeda5875a218df68ec6a445, reversing
    changes made to 382cdc992d17c9b95082ae42ce7d8764dab4bbef.

--- My project/Assets/Scripts/Menus/SettingsMenu.cs
+++ My project/Assets/Scripts/Menus/SettingsMenu.cs
@@ -1,43 +1,11 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
 
 public class SettingsMenu : MonoBehaviour {
     public AudioMixer audioMixer;
     public AudioMixer musicMixer;
-    public TMP_Dropdown resolutionDropdown;
-
-    private Resolution[] resolutions;
-    private void Start()
-    {
-        resolutions = Screen.resolutions;
-        resolutionDropdown.ClearOptions();
-
-        int currectResolutionIndex = 0;
-        List<string> options = new List<string>();
-        for (int i = 0; i < resolutions.Length; i++) {
-            string option = resolutions[i].width + "x" + resolutions[i].height;
-            options.Add(option);
-
-            if (resolutions[i].width == Screen.currentResolution.width && 
-                resolutions[i].height == Screen.currentResolution.height) {
-                currectResolutionIndex = i;
-            }
-        }
-        resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = currectResolutionIndex;
-        resolutionDropdown.RefreshShownValue();
-    }
-
-    public void SetResolution(int resolutionIndex)
-    {
-        Resolution resolution = resolutions[resolutionIndex];
-        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
-    }
-
     public void SetVolume(float volume) {
         audioMixer.SetFloat("Volume", volume);
     }
