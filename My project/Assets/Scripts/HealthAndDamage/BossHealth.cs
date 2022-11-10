278071ecbb23370b9bfc0fa16358da21ed77d8a1
nuflol <abbe2002@outlook.com>
2022-11-10 17:35:06 +0100

    Added score and win screen

--- My project/Assets/Scripts/HealthAndDamage/BossHealth.cs
+++ My project/Assets/Scripts/HealthAndDamage/BossHealth.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.TextCore.Text;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BossHealth : MonoBehaviour
 {
     public int currentHealth;
     public int totalHealth;
+    public GameObject winScreen;
+    public static bool bossIsDead = false;
     private EnemyHealthBar healthBar;
+    public WinScreen WinScript;
+
 
 
 
@@ -16,16 +22,11 @@ public class BossHealth : MonoBehaviour
         currentHealth = totalHealth;
 
         healthBar = GetComponentInChildren<EnemyHealthBar>();
-        healthBar.SetEnemyMaxHealth(totalHealth);
+        //healthBar.SetEnemyMaxHealth(totalHealth);
         healthBar.SetEnemyHealth(currentHealth);
+        GetComponent<WinScreen>().OnWin();
+        
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
 
     public void DamageBoss(int BossDamage)
     {
@@ -34,6 +35,10 @@ public class BossHealth : MonoBehaviour
         {
             FindObjectOfType<AudioManager>().Play("EnemyDead");
             Destroy(gameObject);
+            bossIsDead = true;
+            WinScript.OnWin();
+            Time.timeScale = 0;
+            winScreen.SetActive(true);
         }
         healthBar.SetEnemyHealth(currentHealth);
     }
